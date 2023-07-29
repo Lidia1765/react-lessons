f0846d56ba664e12b9afce7d03dddafc853f0c65
Artem Zelenov <deeloyy@gmail.com>
2023-07-29 13:03:01 +0600

    refactoring

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import './index.scss';
 function App() {
   const [count, setCount] = React.useState(0);
 
-  const onClickPlus = () => {
-    setCount(count+ 1);
+  const handleClickPlus = () => {
+    setCount(count + 1);
   };
-  const onClickMinus= () => {
-    setCount(count- 1);
+
+  const handleClickMinus = () => {
+    setCount(count - 1);
   };
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
         <h1>{count}</h1>
-        <button onClick={onClickMinus} className="minus">- Минус</button>
-        <button onClick={onClickPlus} className="plus">Плюс +</button>
+        <button onClick={handleClickMinus} className="minus">- Минус</button>
+        <button onClick={handleClickPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );
