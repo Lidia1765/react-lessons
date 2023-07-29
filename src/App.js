6c8935e3e05ae78af6279e0ff14d8bdcf67ae2f2
Artem Zelenov <deeloyy@gmail.com>
2023-07-29 13:30:26 +0600

    refactoring

--- src/App.js
+++ src/App.js
@@ -23,19 +23,19 @@ const questions = [
   },
 ];
 
-function Result({correct}) {
+function Result({ correct }) {
   return (
     <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="" />
       <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
       <a href='/'>
-      <button>Попробовать снова</button>
+        <button>Попробовать снова</button>
       </a>
     </div>
   );
 }
 
-function Game({step, question, onClickVariant}) {
+function Game({ step, question, onClickVariant }) {
   const percentage = Math.round(step / questions.length * 100);
   return (
     <>
@@ -44,8 +44,9 @@ function Game({step, question, onClickVariant}) {
       </div>
       <h1>{question.title}</h1>
       <ul>
-        { question.variants.map((text,index)=> 
-        (<li onClick={()=>onClickVariant(index)} key={text}>{text}</li>))}
+        {question.variants.map((text, index) => (
+          <li onClick={onClickVariant(index)} key={text}>{text}</li>
+        ))}
       </ul>
     </>
   );
@@ -56,19 +57,21 @@ function App() {
   const [correct, setCorrect] = React.useState(0);
   const question = questions[step];
 
-  const onClickVariant = (index) => {
-    setStep (step + 1);
+  const handleClickVariant = (index) => () => {
+    setStep(step + 1);
 
-    if (index == question.correct){
+    if (index === question.correct) {
       setCorrect(correct + 1);
     }
   }
+
   return (
     <div className="App">
-      {
-        step != questions.length ? <Game step={step} question={question}  onClickVariant={onClickVariant}/> : 
-      
-      <Result correct={correct}/> }
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={handleClickVariant} />
+      ) : (
+        <Result correct={correct}/>
+      )}
     </div>
   );
 }
