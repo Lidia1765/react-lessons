c7dab504c0f7887a734038fa933a70f3a5263e02
Lidia <Lkoles2003@mail.ru>
2023-08-13 18:13:55 +0300

    photos

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import { Collection } from './Collection';
 import './index.scss';
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
-
 function App() {
+  const [searchValue, setSearchValue] = React.useState('');
+  const [collections, setCollections] = React.useState([]);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [page, setPage] = React.useState(1);
+  
+  React.useEffect(() => {
+  //setIsLoading(true)
+  fetch('https://64d8d5725f9bf5b879ce9a1d.mockapi.io/collection_photos')
+  .then((res) => res.json())
+  .then((json) => {
+    setCollections(json);
+  })
+  .catch((err) => {
+    console.warn(err);
+    alert('Error');
+  })
+  //.finally( setIsLoading(false));
+  },[]);
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
@@ -27,18 +34,21 @@ function App() {
           <li>Архитектура</li>
           <li>Города</li>
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
+        <input value={searchValue} 
+        onChange={(e) => setSearchValue(e.target.value)} 
+        className="search-input" 
+        placeholder="Поиск по названию" />
       </div>
       <div className="content">
+        {collections.filter((obj) =>  obj.name.toLowerCase()
+          .includes(searchValue.toLowerCase()))
+          .map((obj, index) => (
         <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
+        key={index}
+          name={obj.name}
+          images={obj.photos}
         />
+        ))};
       </div>
       <ul className="pagination">
         <li>1</li>
